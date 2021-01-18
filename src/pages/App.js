62df3525e42202983f3ba23669d6a15ce1754135
Michal O <ostachim98@gmail.com>
2021-01-18 21:24:07 +0100

    add Header.js Page.js and modify App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import '../styles/App.css';
+import Header from './Header';
+import Page from './Page';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          Aplikacja
+          <Header/>
+          <Page/>
         </div>
       </Router>
     
