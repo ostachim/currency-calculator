c3a7ccc9fe06f525136904b9677115a196d7765c
Michal O <ostachim98@gmail.com>
2021-01-26 23:57:16 +0100

    Updates

---
+++ precache-manifest.403678c2e76e30192d811f684b2357e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dd328468ee6f74520db",
+    "url": "/currency-calculator/static/css/main.f29068d1.chunk.css"
+  },
+  {
+    "revision": "3dd328468ee6f74520db",
+    "url": "/currency-calculator/static/js/main.74ab304a.chunk.js"
+  },
+  {
+    "revision": "778795ff38cb6f712d0b",
+    "url": "/currency-calculator/static/js/runtime~main.a8f5e66f.js"
+  },
+  {
+    "revision": "e8c8603251917f585c29",
+    "url": "/currency-calculator/static/js/2.32a94c25.chunk.js"
+  },
+  {
+    "revision": "202d2d6f1dd755feaee15e786d6e77de",
+    "url": "/currency-calculator/index.html"
+  }
+];
